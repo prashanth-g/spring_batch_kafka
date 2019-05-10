8727b247bb2ba6c6d064d5de99cf736dea65fd0c
prashanth-g <prashanthcsgce@gmail.com>
2019-05-10 18:45:34 +0530

    boot class update

--- src/main/java/com/prashanth/spring/batch/kafka/BatchKafkaApplication.java
+++ src/main/java/com/prashanth/spring/batch/kafka/BatchKafkaApplication.java
@@ -1,4 +1,11 @@
 package com.prashanth.spring.batch.kafka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BatchKafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BatchKafkaApplication.class, args);
+    }
 }
