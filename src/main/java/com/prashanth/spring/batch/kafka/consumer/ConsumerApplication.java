dd9bea53995da01a8000690e82dea93541adbf50
prashanth-g <prashanthcsgce@gmail.com>
2019-05-16 20:32:37 +0530

    Added consumer itemreader

--- src/main/java/com/prashanth/spring/batch/kafka/consumer/ConsumerApplication.java
+++ src/main/java/com/prashanth/spring/batch/kafka/consumer/ConsumerApplication.java
@@ -2,16 +2,26 @@ package com.prashanth.spring.batch.kafka.consumer;
 
 import com.prashanth.spring.batch.kafka.model.Customer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.kafka.KafkaItemReader;
+import org.springframework.batch.item.kafka.builder.KafkaItemReaderBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+import java.util.Properties;
+
+@Log4j2
 @SpringBootApplication
 @EnableBatchProcessing
 @RequiredArgsConstructor
@@ -22,6 +32,7 @@ public class ConsumerApplication {
 
     private final StepBuilderFactory stepBuilderFactory;
     private final JobBuilderFactory jobBuilderFactory;
+    private final KafkaProperties kafkaProperties;
 
     @Bean
     Job job() {
@@ -32,10 +43,33 @@ public class ConsumerApplication {
     }
 
     @Bean
+    KafkaItemReader <Long, Customer> kafkaItemReader() {
+        Properties properties = new Properties();
+        properties.putAll(this.kafkaProperties.buildConsumerProperties());
+        return new KafkaItemReaderBuilder<Long, Customer>()
+                .partitions(1)
+                .consumerProperties(properties)
+                .name("customers-reader")
+                .saveState(true)
+                .topic("customers")
+                .build();
+    }
+
+    @Bean
     Step start() {
+
+        ItemWriter itemWriter = new ItemWriter<Customer>() {
+            @Override
+            public void write(List<? extends Customer> items) throws Exception {
+                items.forEach(item -> log.info("new customer " + item));
+            }
+        };
+
         return stepBuilderFactory
                 .get("step")
                 .<Customer, Customer>chunk(10)
+                .writer(itemWriter)
+                .reader(kafkaItemReader())
                 .build();
     }
 }
